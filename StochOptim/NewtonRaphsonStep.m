a82b37bd8239db37baa69112925b97a9d3842cf0
david.kasto <david.kasto@gmail.com>
2018-09-17 14:06:49 +0200

    Moved old stuff into subfolder

--- StochOptim/NewtonRaphsonStep.m
+++
@@ -1,5 +0,0 @@
-function xNew = NewtonRaphsonStep(xCurrent, firstDerivativeValue, secondDerivativeValue)
-% Carries out a Newton-Raphson iteration step, taking three inputs, xj, 
-% f′(xj), and f′′(xj), and returning x(j+1).
-    xNew = xCurrent - firstDerivativeValue / secondDerivativeValue;
-end
